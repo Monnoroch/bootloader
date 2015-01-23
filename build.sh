3cdd51bc7f246b3a080325d94b445bd44769fd00
Max <monnoroch@gmail.com>
2015-01-23 16:50:34 +0300

    Removed comments

--- build.sh
+++ build.sh
@@ -1,18 +1,24 @@
+# clear
 rm *.bin *.img *.o
+
+# compile
 nasm boot.asm -f bin -o boot.bin
-# gcc -m32 -ffreestanding -c main.c -o main.o
-# gcc -m32 -static -nostartfiles -c video.c -o video.o
-# gcc -m32 -static -nostartfiles -c ports.c -o ports.o
+gcc -m32 -ffreestanding -c main.c -o main.o
+gcc -m32 -static -nostartfiles -c video.c -o video.o
+gcc -m32 -static -nostartfiles -c ports.c -o ports.o
 
-# ld -melf_i386 -N -Ttext 0x01000 -e0x01000 -o kernel.o main.o video.o ports.o
+# link
+ld -melf_i386 -N -Ttext 0x01000 -e0x01000 -o kernel.o main.o video.o ports.o
 
-# objcopy -R .note -R .comment -S -O binary kernel.o kernel.bin
+# magic
+objcopy -R .note -R .comment -S -O binary kernel.o kernel.bin
 
-# BOOT_SIZE=$(stat -c %s boot.bin)
-# KERNEL_SIZE=$(stat -c %s kernel.bin)
-# SUM_SIZE=$(($BOOT_SIZE + $KERNEL_SIZE))
-# DUMMY_SIZE=$(./dummy_size.py $SUM_SIZE)
-# head -c $DUMMY_SIZE < /dev/zero > tmp
+# combine
+BOOT_SIZE=$(stat -c %s boot.bin)
+KERNEL_SIZE=$(stat -c %s kernel.bin)
+SUM_SIZE=$(($BOOT_SIZE + $KERNEL_SIZE))
+DUMMY_SIZE=$(./dummy_size.py $SUM_SIZE)
+head -c $DUMMY_SIZE < /dev/zero > tmp
 
-# cat boot.bin kernel.bin tmp > kernel.img
-# rm tmp
+cat boot.bin kernel.bin tmp > kernel.img
+rm tmp
